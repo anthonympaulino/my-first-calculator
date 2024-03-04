beb5c3ce332263de2047ea4fa0005d4601f17e57
anthonympaulino <160381299+anthonympaulino@users.noreply.github.com>
2024-03-04 20:50:27 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+print("What do you want to calculate today?")
+print("The format x (operation) y is used")
+x = int(input("x = "))
+y = int(input("y = "))
+print("what operation do you want to perform on the two numbers?")
+operation = input(" ")
+if operation == "*":
+    print(x * y)
+elif operation == "+":
+    print(x + y)
+elif operation == "-":
+    print(x - y)
+elif operation == "/":
+    print(x / y)
+
+             
